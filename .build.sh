863eb0b77298db0f143a22ce6f2675661115e1fa
David Nedved <david@nedved.com.au>
2024-03-12 23:20:01 +1000

    update example config in docker compose

--- .build.sh
+++ .build.sh
@@ -23,7 +23,8 @@ esac
 
 
 curl -L --connect-timeout 5 --max-time 1800 \
-    "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Froute53&idempotency=98170178482060" \
+    "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Fcloudflare&p=github.com%2Fcaddy-dns%2Froute53&p=github.com%2Fcaddy-dns%2Fdigitalocean&p=github.com%2Fcaddy-dns%2Fgoogleclouddns&p=github.com%2Fcaddy-dns%2Fazure&idempotency=65708359145450"
     -o /usr/bin/caddy && chmod +x /usr/bin/caddy
 
+# "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Froute53&idempotency=98170178482060" \
 # "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Fcloudflare&p=github.com%2Fcaddy-dns%2Froute53&p=github.com%2Fcaddy-dns%2Fdigitalocean&p=github.com%2Fkirsch33%2Frealip&p=github.com%2Fcaddy-dns%2Fgoogleclouddns&p=github.com%2Fcaddy-dns%2Fazure&p=github.com%2FWingLim%2Fcaddy-webhook&p=github.com%2Fmohammed90%2Fcaddy-storage-loader&idempotency=42864420815349"
