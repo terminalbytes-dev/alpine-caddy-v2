a4b5dc1a520efbf6385a4c1618f7975232ec4bfd
David Nedved <david@nedved.com.au>
2024-03-12 22:16:18 +1000

    update build workflow

--- .build.sh
+++ .build.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-CADDY_ARCH=""
+OS_ARGS=""
 
 case "$TARGETPLATFORM" in
   "linux/amd64")
-    CADDY_ARCH="linux/amd64"
+    OS_ARGS="os=linux&arch=amd64"
     ;;
 
   "linux/arm/v6")
-    CADDY_ARCH="linux/arm6"
+    OS_ARGS="os=linux&arch=arm&arm=6"
     ;;
 
   "linux/arm/v7")
-    CADDY_ARCH="linux/arm7"
+    OS_ARGS="os=linux&arch=arm&arm=7"
     ;;
 
   "linux/arm64")
-    CADDY_ARCH="linux/arm64"
+    OS_ARGS="os=linux&arch=arm64"
     ;;
 
-  "linux/386")
-    CADDY_ARCH="linux/386"
-    ;;
 esac
 
 
 curl \
     --connect-timeout 5 \
     --max-time 1800 \
-    "https://caddyserver.com/download/${CADDY_ARCH}?plugins=dns,docker,hook.service,http.awslambda,http.cache,http.cgi,http.cors,http.expires,http.filter,http.forwardproxy,http.ipfilter,http.mailout,http.nobots,http.permission,http.ratelimit,http.realip,http.s3browser,http.webdav,net,supervisor,tls.dns.azure,tls.dns.cloudflare,tls.dns.digitalocean,tls.dns.dyn,tls.dns.godaddy,tls.dns.googlecloud,tls.dns.lightsail,tls.dns.linode,tls.dns.ns1,tls.dns.rfc2136,tls.dns.route53&license=personal&telemetry=off" \
+    "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Froute53&idempotency=98170178482060" \
     | tar --no-same-owner -C /usr/bin/ -xz caddy
+
+# "https://caddyserver.com/api/download?${OS_ARGS}&p=github.com%2Fcaddy-dns%2Fcloudflare&p=github.com%2Fcaddy-dns%2Froute53&p=github.com%2Fdarkweak%2Fsouin%2Fplugins%2Fcaddy&p=github.com%2Faksdb%2Fcaddy-cgi%2Fv2&p=github.com%2Fcaddy-dns%2Fdigitalocean&p=github.com%2Fkirsch33%2Frealip&p=github.com%2FRussellLuo%2Fcaddy-ext%2Fratelimit&p=github.com%2Fsjtug%2Fcaddy2-filter&p=github.com%2Fcaddy-dns%2Fgoogleclouddns&p=github.com%2Fcaddy-dns%2Fazure&p=github.com%2Fbaldinof%2Fcaddy-supervisor&p=github.com%2Ftosie%2Fcaddy-dns-linode&p=github.com%2Fgreenpau%2Fcaddy-lambda&idempotency=54678585182523" \
